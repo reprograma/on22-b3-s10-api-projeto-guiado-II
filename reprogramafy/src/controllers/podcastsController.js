c9c0b2ea4905e7f06ba01365106c8a24b7945cbd
Cilene <cilene.kinomoto@gmail.com>
2023-05-27 18:28:09 -0300

    Projeto Guiado Pronto_sem10 - JucileneBratti

---
+++ reprogramafy/src/controllers/podcastsController.js
@@ -0,0 +1,98 @@
+const podsJson = require("../models/podcasts.json")
+
+const getAllPodcasts = (req,res)=>{
+    try {
+        res.status(200).json([{
+            podcasts: podsJson
+        }])
+    } catch (err) {
+        res.status(500).send([{
+            message: 'Erro no server'
+        }])
+    }
+}
+
+const getTopics = (req, res) =>{ //add um tolowerCase
+    const topicRequest = req.query.topic;
+    const topicFilter = podsJson.filter((pods)=> pods.topic.includes(topicRequest))
+    if (topicFilter.length > 0) {
+        res.status(200).send(topicFilter)
+    } else {
+        res.status(404).send([{
+            message: "Podcast não encontrado!! / Not Found!!"
+        }])
+    }
+}
+
+const addPods = (req, res) => {
+    try {
+        let nameRequest = req.body.name;
+        let podcasterRequest = req.body.podcaster;
+        let topicRequest = req.body.topic;
+        let starsRequest = req.body.stars;
+        
+        let newPodcast = {
+            id: Math.floor(Date.now() * Math.random()).toString(36),
+            name: nameRequest,
+            podcaster: podcasterRequest,
+            topic: topicRequest,
+            stars: starsRequest
+        }
+        podsJson.push(newPodcast);
+        res.status(201).json([{
+            message: "Novo Podcast cadastrado!!",
+            newPodcast
+        }])
+    } catch (error) {
+        console.log(error)
+        res.status(500).send([{
+            message: "Erro interno ao cadastrar!!"
+        }])
+    }
+}
+
+
+const updatePods = (req, res) => {
+    const idRequest = req.params.id 
+    const starsRequest = req.body.stars 
+    starsFind = podsJson.find((podcast) => podcast.id == idRequest)
+
+    if (starsFind) {
+        starsFind.stars = starsRequest
+        res.status(200).json([{
+            message: "Classificação atualizada com sucesso!!!",
+            podsJson
+        }])
+    } else {
+        res.status(404).json([{
+            message: "Não foi modificado!!!"
+        }])
+    }
+}
+
+const deletePods = (req, res) =>{
+    const idRequest = req.params.id
+    const indicePods = podsJson.findIndex((podcast) => podcast.id == idRequest) 
+
+    podsJson.splice(indicePods, 1)
+
+    if (indicePods) {
+        res.status(200).json([{
+            message: "O podcast deletado!!",
+            "podcast deletado": idRequest, 
+            podsJson
+        }])
+    } else {
+        res.status(404).send([{
+            message: "Podcast não deletado!!"
+        }])
+    }
+}
+
+module.exports = {
+    getAllPodcasts,
+    getTopics,
+    addPods,
+    updatePods,
+    deletePods
+}
---
+++ reprogramafy/src/controllers/songsController.js
@@ -0,0 +1,136 @@
+const songsJson = require("../models/songs.json") // conectando o controllers com as rotas e app
+
+//const express = require("express")
+
+const getAllSongs = (req, res)=>{
+    try {
+        res.status(200).json([{
+            songs: songsJson,
+        }])
+    } catch (error) {
+        res.status(500).send([{
+            message: 'Erro no Server'
+        }])
+    }
+}
+
+const getSongs = (req, res)=>{
+    const songRequest = req.params.id
+    const songFilter = songsJson.filter(song => song.id == songRequest)
+    if(songFilter.length > 0){
+        res.status(200).send(songFilter)
+    } else {
+        res.status(404).send(([{
+            message: "Musica não encontrada!!! / Not Found!!!"
+        }]))
+    }
+}
+
+
+const getArtit = (req, res) =>{
+    let artistsRequest = req.query.artists.toLowerCase()
+    let artistsFilter = songsJson.filter((song) =>{
+        artistsLowerCase = song.artists.map((artistsArray)=> artistsArray.toLowerCase())
+        return artistsLowerCase.includes(artistsRequest)
+    })
+
+    console.log(artistsFilter);
+    if (artistsFilter.length > 0) {
+        res.status(200).send(artistsFilter)
+    } else {
+        res.status(404).send([{
+            message: "Artista não encontrado!!! / Not Found!!!"
+        }])
+    }
+}
+
+const addSong = (req, res) =>{
+    try {
+        let titleRequest = req.body.title;
+        let launchYearRequest = req.body.launchYear;
+        let favoritedRequest = req.body.favorited;
+        let artistsRequest = req.body.artists;
+
+        let newSong = {
+            id: Math.floor(Date.now() * Math.random()).toString(36),
+            title: titleRequest,
+            launchYear: launchYearRequest,
+            favorited: favoritedRequest,
+            artists: artistsRequest
+        };
+        songsJson.push(newSong)
+        res.status(201).json([{
+            message: "Nova música cadastrada!!!/ Add New Song!!",
+            newSong
+        }])
+    } catch (error) {
+        console.log(error)
+        res.status(500).send([{
+            message: "Erro interno ao cadastrar!!!"
+        }])
+    } 
+}
+
+const updateSong = (req, res) =>{
+    const idRequest = req.params.id 
+    let songRequest = req.body
+    let findSong = songsJson.findIndex((song) => song.id == idRequest)
+
+    if (songsJson.splice(findSong, 1, songRequest)) {
+        res.status(200).json([{
+            message: "Musica atualizada com sucesso!!/ Update songs sucess!!",
+            songsJson
+        }])
+    } else {
+        res.status(404).send([{
+            message: "Musica não encontrado!!"
+        }])
+    }
+}
+
+const deleteSong = (req, res) =>{
+    const idRequest = req.params.id 
+    const songFilter = songsJson.findIndex((song) => song.id == idRequest)
+    songsJson.splice(songFilter, 1)
+    if (songFilter) {
+        res.status(200).json([{
+            message: "A musica selecionada foi deletada!!",
+            "musica deletada": idRequest, 
+            songsJson
+        }])
+    } else {
+        res.status(404).send([{
+            message: "Musica não deletada!!!"
+        }])
+    }
+} 
+
+const updateFav = (req, res) =>{
+    const idRequest = req.params.id 
+    const favoritedRequest = req.body.favorited 
+    favoritedFind = songsJson.find((song) => song.id == idRequest) 
+
+    if (favoritedFind) {
+        favoritedFind.favorited = favoritedRequest,
+        res.status(200).json([{
+            message: "Classificação atualizada com Sucesso!!"
+        }])
+    } else {
+        res.status(404).json([{
+            message: "Não quero hoje!!!"
+        }])
+    }
+}
+
+
+
+module.exports = {
+    getAllSongs,
+    getSongs,
+    getArtit,
+    addSong,
+    updateSong,
+    deleteSong,
+    updateFav
+}
+
