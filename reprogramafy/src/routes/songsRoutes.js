3c34742ea9f56d5a503bcc62ee6d1806d8ec80d0
Leticia <leticiasrogerio@gmail.com>
2023-05-31 21:34:50 -0300

    projeto guiado

---
+++ reprogramafy/src/routes/songsRoutes.js
@@ -0,0 +1,14 @@
+const songsController = require("../controllers/songsController") //importa o controller
+const express = require("express") //importa o express
+const router = express.Router() //função de rotas do express
+
+//router. metodo http(rota, função)
+router.get("/library", songsController.getAllSongs);
+router.get("/songs/:id", songsController.getSong);
+router.get("/artists", songsController.getArtist);
+router.post("/add", songsController.addSong);
+router.put("/update/:id", songsController.updateSong);
+router.delete("/delete/:id", songsController.deleteSong);
+router.patch("/favorited/:id", songsController.updateFav);
+
+module.exports = router
